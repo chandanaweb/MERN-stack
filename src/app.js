8d19e90ab285e0483facd9c677bfc746e98aeff7
chandana malepati <mchandana.1288@gmail.com>
2020-05-09 20:24:04 -0500

    Express configuration for server side

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const title = 'Hello World! ';
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: path.resolve(__dirname, './src')+ '/app.js',
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.join(__dirname, "public"),
+        filename: "bundle.js",
+        publicPath: "/"
   }
 };
